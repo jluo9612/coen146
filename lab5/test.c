63f7808cb2572792e7a1ad9325ac63601bbf15bf
jluo9612 <luojiabei@gmail.com>
2018-03-11 11:18:09 -0700

    Fix address parsing

--- lab5/test.c
+++ lab5/test.c
@@ -16,23 +16,24 @@
 #include <stdlib.h>
 
 /*
-0 router id
-1 number of nodes
-2 table cost file
-3 host table file
+0 executable
+1 router id
+2 number of nodes
+3 table cost file
+4 host table file
 */
+
+#define ROW 4
+#define COL 4
+
+int cmat[ROW][COL];
+int count = 0;
 
 typedef struct machine {
 	char name[50];
 	char IP[50];
 	int port;
 }machine_t;
-
-// struct to pass cmat to receive_info
-typedef struct matstr {
-  	int m, n;
-  	int * mat;
-}matstr_t;
 
 pthread_mutex_t myMutex;
 
@@ -45,27 +46,21 @@ void printHelp (int row, int col, int cmat[][col]) {
 }
 
 // thread 1 func
-void* receive_info(void* cmat) {
+void* receive_info() {
 	// receive array
 	int array[3];
 	int arrayReceived[3];
-
-	matstr_t *M = (matstr_t*)cmat;
-	int m = (*M).m;
-	int n = (*M).n;
-	int* mat = (*M).mat;
 	int sock;
-	while ((recvfrom(sock, arrayReceived, sizeof(arrayReceived), 0, NULL, NULL)) < 0) sleep(10);
-	
+	while ((recvfrom(sock, arrayReceived, sizeof(arrayReceived), 0, NULL, NULL)) <= 0) sleep(10); 
 	for (int i = 0 ; i < 100 ; ++i) array[i] = ntohl(arrayReceived[i]);
+
 	pthread_mutex_lock(&myMutex);
 	// write to cost table
-	*(mat + array[0]*n + array[1]) = array[2];
-	*(mat + array[1]*n + array[0]) = array[2];
-
+	cmat[array[0]][array[1]] = 11;
+	cmat[array[1]][array[0]] = 11;
+	count += 1;
 	pthread_mutex_unlock(&myMutex);
-	printf("Cost table changed");
-	// printHelp(m, n, mat);
+
 	return NULL;
 }
 
@@ -88,17 +83,16 @@ main (int argc, char *argv[]) {
 	}
 	// initialize cost and hosts matrices
 	machine_t myMachines[4];
-	int cmat[atoi(argv[2])][atoi(argv[2])];
 
 	// parse files
 	FILE* cost = fopen(argv[3], "rw");
 	if (cost == NULL) {
-		printf("Can't open output file!\n");
+		printf("Can't open cost file!\n");
 		return 1;
 	}
 	FILE* hosts = fopen(argv[4], "r");
 	if (hosts == NULL) {
-		printf("Can't open output file!\n");
+		printf("Can't open hosts file!\n");
 		return 1;
 	}
 	int i, j, in = 0;
@@ -111,45 +105,46 @@ main (int argc, char *argv[]) {
 	}
 	fclose(cost);
 
+	// not parsed correctly
 	int m, portn = 0;
 	char nameb[50];
 	char IPb[50];
 	while (!feof(hosts) && m < atoi(argv[2])) {
-		printf("%s %s %d\n", nameb, IPb, portn);
 		fscanf(hosts, "%s %s %d", nameb, IPb, &portn);
 		machine_t machine;
 		strcpy(machine.name, nameb);
 		strcpy(machine.IP, IPb);
 		machine.port = portn;
 		myMachines[m] = machine;
+		m++;
 	}
 	fclose(hosts);
 
-	// initialize mat struct
-	matstr_t M;
-	M.mat = cmat;
-	M.m = atoi(argv[2]);
-	M.n = atoi(argv[2]);
+	printf("Cost table looks like this initially:\n");
+	printHelp(ROW, COL, cmat);
 
 	// initialize threads
 	int rc1, rc3;
 	pthread_t thread1, thread3;
 
-	printf("Cost table looks like this initially:\n");
-	printHelp(atoi(argv[2]), atoi(argv[2]), cmat);
+	// start threads
+	if( (rc1 = pthread_create( &thread1, NULL, &receive_info, (void*)cmat) ) )
+	{
+		printf("Thread creation failed: %d\n", rc1);
+	}
 
-	int count = 0;
-	while (count < 2) {
+	// if( (rc3 = pthread_create( &thread3, NULL, &link_state, NULL)) )
+	// {
+	// 	printf("Thread creation failed: %d\n", rc3);
+	// } 
 
-		if( (rc1 = pthread_create( &thread1, NULL, &receive_info, (void*)(&M)) ) )
-		{
-			printf("Thread creation failed: %d\n", rc1);
-		}
+	int ma;
+	for (ma = 0; ma < 4; ma++) {
+		printf("Machine: %d %s %s %d\n", ma, myMachines[ma].name, myMachines[ma].IP, myMachines[ma].port);
+	}
 
-		if( (rc3 = pthread_create( &thread3, NULL, &link_state, NULL)) )
-		{
-			printf("Thread creation failed: %d\n", rc3);
-		} // thread3 started
+	int ct = 0;
+	while (ct < 2) {
 
 		// takes keyboard input
 		int snode, ncost;
@@ -162,7 +157,7 @@ main (int argc, char *argv[]) {
 			int msg[3] = {atoi(argv[1]), snode, ncost}; 
 			int msgToSend[3];
 			for (i = 0; i < 3; ++i) msgToSend[i] = htonl(msg[i]);
-			// --------------sending message--------------
+
 			struct sockaddr_in serverAddr; 
 			socklen_t addr_size;
 			int sock;
@@ -174,28 +169,30 @@ main (int argc, char *argv[]) {
 			memset (serverAddr.sin_zero, '\0', sizeof (serverAddr.sin_zero));  
 			addr_size = sizeof serverAddr;
 
-			// Create socket
+			// Create socket and binding
 			if ((sock = socket (PF_INET, SOCK_DGRAM, 0)) == -1) printf("Error creating socket\n");
-
 			if ((bind(sock, (struct sockaddr *) &serverAddr, sizeof(serverAddr))) == -1) printf("Error binding\n");
-
 			printf("Config success, socket created, addr binded\n");
 
 			// send messages
 			printf ("Sending...\n");
+			// server address is wrong?
 			while (sendto (sock, &msgToSend, sizeof(msgToSend), 0, (struct sockaddr *)&serverAddr, addr_size) == -1) printf("Sending failed. Please restart\n");
 			printf ("Packet sent.\n");
 		}
-		count++;
+		ct++;
 		// time buffer for thread 1 to be done
 		sleep(10);
+		// should received at this point?
+		printf("Count : %d\n", count);
 		printf("Cost table looks like this now:\n");
 		printHelp(atoi(argv[2]), atoi(argv[2]), cmat);
 	}	
 	pthread_join(thread1, NULL);
 	pthread_join(thread3, NULL);
+
 	// main thread waits 30 sec and finish here
-	sleep(30);
+	sleep(2);
 
 	return 0;	
 }
